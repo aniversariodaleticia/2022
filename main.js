3b264745e5ece34f1d604f3f961543bead76a450
lucas-rl <lucasrl55@gmail.com>
2022-10-17 20:14:43 -0300

    Convite thalita

--- main.js
+++ main.js
@@ -6,24 +6,24 @@ function pegarVotos(){
     return JSON.parse(request.responseText)
 }
 
-function votar(key){
-    votos = pegarVotos()
+function votar(){
+    nome = document.getElementById('nome').innerHTML
+    voto = document.querySelector("input[name='sabores']:checked").value
+    console.log(nome)
+    console.log(voto)
+    document.getElementById('botao').innerHTML = "Confirmado!!";
+    document.getElementById('botao').disabled = true;
+    
+    comeco = "{\""
 
-    for (var j in votos){
-        if (j == key){
-            votos[j] += 1
-        }
-    }
+    json = comeco.concat(nome, "\":\"", voto, "\"}" )
     
     url = "https://getpantry.cloud/apiv1/pantry/8df87e2e-cdbd-4832-b741-51fea8f1fc1c/basket/votacaoPicole"
     let request = new XMLHttpRequest()
-    request.open("POST", url, false)
+    request.open("PUT", url, false)
     request.setRequestHeader('Content-Type', 'application/json')
-    request.send(JSON.stringify(votos))
+    request.send(json)
 }
 
 
-console.log(pegarVotos())
-votar("brigadeiro")
-console.log(pegarVotos())
 
